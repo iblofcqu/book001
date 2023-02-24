8451ce552a73ccb63022423bfa41c8d6bc57bc00
兰昊 <bigpangl@163.com>
2023-02-24 14:57:03 +0800

    docs(init): sphinx 文档仓库初始化配置

---
+++ COMMANDS.md
@@ -0,0 +1,11 @@
+## 安装依赖库
+
+```shell
+pip install -r requirements.txt
+```
+
+## 生成静态文件
+
+```shell
+sphinx-build -b html source html
+```
---
+++ source/conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = '深度学习再建筑工程中的应用'
+copyright = '2023, 重庆大学智能建造实验室'
+author = '重庆大学智能建造实验室'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+html_static_path = ['_static']
